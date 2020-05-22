ea2afd81180121be534155a2a77156bee5a71729
Elias Snz <eliaas.saanchez@gmail.com>
2020-05-22 15:27:15 -0500

    Final Commit

--- lib/posts_detail.dart
+++ lib/posts_detail.dart
@@ -1,11 +1,52 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:postsapp/comments_class.dart';
 import 'package:postsapp/post_class.dart';
 
-int identifier;
+Future<List<Comment>> fetchPhotos(http.Client client, var url) async {
+  final response =
+  await client.get(url);
+
+  // Use the compute function to run parsePhotos in a separate isolate.
+  return compute(parsePhotos, response.body);
+}
+
+// A function that converts a response body into a List<Photo>.
+List<Comment> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Comment>((json) => Comment.fromJson(json)).toList();
+}
+
+class Comment {
+  int postId;
+  int id;
+  String name;
+  String email;
+  String body;
+
+  Comment({this.postId, this.id, this.name, this.email, this.body});
+
+  Comment.fromJson(Map<String, dynamic> json) {
+    postId = json['postId'];
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['postId'] = this.postId;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['body'] = this.body;
+    return data;
+  }
+}
 
 class PostsDetail extends StatefulWidget {
   final Post post;
@@ -16,43 +57,87 @@ class PostsDetail extends StatefulWidget {
 }
 
 class _PostsDetailState extends State<PostsDetail>{
-  String url = "https://jsonplaceholder.typicode.com/comments?postId=1";
-  CommentsClass commentsClass;
-
+  var url = "https://jsonplaceholder.typicode.com/comments?postId=";
   @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
+  Widget build(BuildContext context) {
+    url += widget.post.id.toString();
 
-  void fetchData() async {
-    var response = await http.get(url);
-    var decodedJson = jsonDecode("{\"comments\":" + response.body +"}");
-    commentsClass = CommentsClass.fromJson(decodedJson);
-    print(commentsClass.toString());
-    setState(() {});
+    return Scaffold(
+        backgroundColor: Color(0xff000000),
+      appBar: AppBar(
+        backgroundColor: Color(0xff1e2d3b),
+        title: Text("Comments"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Text(widget.post.title, style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white, fontFamily: 'Garlic Sans',fontSize: 28),),
+                  SizedBox(height: 15),
+                  Text(widget.post.body, style: TextStyle(
+                      color: Colors.white, fontFamily: 'Garlic Sans',fontSize: 24),),
+                ],
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.orange,
+                  width: 2.0,
+                ),
+                color: Color(0xff101823),
+                borderRadius: BorderRadius.circular(10),
+                  )
+              ),
+            SizedBox(height: 20),
+            Text("Comments",style: TextStyle(
+                color: Colors.white, fontFamily: 'Garlic Sans',fontSize: 20),),
+            Expanded(
+                child: FutureBuilder<List<Comment>>(future:fetchPhotos(http.Client(),url), builder:(context, snapshot) {
+                        if (snapshot.hasError) print(snapshot.error);
+                        return snapshot.hasData ? CommentsList(comments: snapshot.data) : Center(child: CircularProgressIndicator());
+                      },
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class CommentsList extends StatelessWidget {
+  final List<Comment> comments;
+  CommentsList({Key key, this.comments}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    url = "https://jsonplaceholder.typicode.com/comments?postId=" + widget.post.id.toString();
-    print(url);
-    //fetchData();
-
-    return Scaffold(
-        drawer: Drawer(),
-        body: commentsClass == null ? Center(child: CircularProgressIndicator(),) : ListView(
-          children: <Widget>[
-            Text(widget.post.title),
-            Text(widget.post.body),
-            Row(
-              children: commentsClass.comments.map((e) => ListTile(
-                title: Text(e.email),
-                subtitle: Text(e.body),
-              )).toList(),
+    return ListView.builder(
+      //gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+      itemCount: comments.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Container(
+            child: ListTile(
+              title: Text("From: " + comments[index].email, style: TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.white, fontFamily: 'Philosopher',fontSize: 16),),
+              subtitle: Text(comments[index].body, style: TextStyle(
+                  color: Colors.white, fontFamily: 'Philosopher',fontSize: 12),),
             ),
-          ],
-        )
+            decoration: BoxDecoration(
+                color: Color(0xff101823),
+                border: Border(left: BorderSide(color: Colors.grey, width: 4.0),
+                )
+            ),
+          ),
+        );
+          //Text(comments[index].email);
+      },
     );
   }
 }
--- lib/posts_list.dart
+++ lib/posts_list.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:ui';
 
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:postsapp/post_class.dart';
 import 'package:postsapp/posts_detail.dart';
@@ -30,26 +33,30 @@ class _PostsListState extends State<PostsList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff000000),
       appBar: AppBar(
-        title: Text("Posts"),
-        backgroundColor: Colors.grey,
+        title: Center(child: Text("POSTS")),
+        backgroundColor: Color(0xff1e2d3b),
       ),
-      drawer: Drawer(),
       body: postClass == null ? Center(child: CircularProgressIndicator(),): ListView(
         children: postClass.post.map((e) => InkWell(
           onTap: () {
             Navigator.push(context,MaterialPageRoute(builder: (context) => PostsDetail(post: e)));
           },
-          child: Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Card(
-              elevation: 3.0,
+          child:  Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
               child: ListTile(
-                leading: Icon(Icons.account_circle,size: 40,),
-                title: Text(e.title, style: TextStyle(fontWeight: FontWeight.bold),),
-                subtitle: Text(e.body.substring(0,30) + "..."),
+                title: Text(e.title, style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.white, fontFamily: 'Philosopher',fontSize: 20),),
+                trailing: Icon(Icons.keyboard_arrow_right, color: Color(0xffbbbbbb),),
+                ),
+              decoration: BoxDecoration(
+                  color: Color(0xff101823),
+                  border: Border(left: BorderSide(color: Colors.orange, width: 4.0),
+                )
               ),
-            ),
+              ),
           ),
         )).toList(),
       )
